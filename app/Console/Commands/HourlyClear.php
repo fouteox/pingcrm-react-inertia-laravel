14c78d7ab3201d182ad6ae1a675806c989fd84d4
Florian <fouteox@gmail.com>
2022-11-24 23:56:06 +0100

    make schedule clear hourly

---
+++ app/Console/Commands/HourlyClear.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
+
+class HourlyClear extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'clear:hourly';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear DB and storage every hourly.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Artisan::call('down');
+
+        Storage::deleteDirectory('users');
+        Storage::deleteDirectory('.glide-cache');
+        Artisan::call('migrate:fresh --force --seed');
+        Artisan::call('up');
+        
+        return Command::SUCCESS;
+    }
+}
