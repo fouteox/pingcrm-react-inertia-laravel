5b0d7b6b6b4199e05b2bbefa672d734716e04314
Florian <fouteox@gmail.com>
2022-11-27 11:31:22 +0100

    fix hidden nav mobile

--- resources/js/Shared/MainMenu.jsx
+++ resources/js/Shared/MainMenu.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import MainMenuItem from '@/Shared/MainMenuItem';
+import React from "react";
+import MainMenuItem from "@/Shared/MainMenuItem";
 
-export default ({ className }) => {
-  return (
-    <div className={className}>
-      <MainMenuItem text="Dashboard" link="dashboard" icon="dashboard" />
-      <MainMenuItem text="Organizations" link="organizations.index" icon="office" />
-      <MainMenuItem text="Contacts" link="contacts.index" icon="users" />
-      <MainMenuItem text="Reports" link="reports" icon="printer" />
-    </div>
-  );
+export default ({ className, onClick }) => {
+    return (
+        <div className={className} onClick={onClick}>
+            <MainMenuItem text="Dashboard" link="dashboard" icon="dashboard" />
+            <MainMenuItem
+                text="Organizations"
+                link="organizations.index"
+                icon="office"
+            />
+            <MainMenuItem text="Contacts" link="contacts.index" icon="users" />
+            <MainMenuItem text="Reports" link="reports" icon="printer" />
+        </div>
+    );
 };
