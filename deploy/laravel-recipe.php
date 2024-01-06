871c3df6194564c3db870d865abcc0810471ffbf
Florian <fouteox@gmail.com>
2024-01-06 09:01:20 +0100

    simplify connexion with deployer

--- deploy/laravel-recipe.php
+++ deploy/laravel-recipe.php
@@ -11,8 +11,5 @@ Dotenv::createImmutable(__DIR__ . '/../')->load();
 desc('Inject all necessary .env variables inside deployer config');
 task('deploy:env', function () {
     Dotenv::createImmutable(__DIR__ . '/../')->load();
-    set('remote_user', env('APP_USER'));
-    set('hostname', env('APP_IP'));
-    set('port', env('APP_PORT'));
     set('password', env('APP_SUDO_PWD'));
 });
