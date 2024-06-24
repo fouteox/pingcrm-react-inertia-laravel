2259492c88cffa1d885fa29d4e29e021ad12e51c
Florian <fouteox@gmail.com>
2024-06-24 14:05:42 +0200

    Fixed an issue that caused toasts to be re-rendered when returning to a backward page

--- resources/js/Shared/FlashMessages.jsx
+++ resources/js/Shared/FlashMessages.jsx
@@ -1,20 +1,39 @@
-import { usePage } from "@inertiajs/react";
-import { useEffect } from "react";
+import {router, usePage} from "@inertiajs/react";
+import {useCallback, useEffect, useRef} from "react";
 import toast, { Toaster } from "react-hot-toast";
 
 export default () => {
     const { flash, errors } = usePage().props;
     const numOfErrors = Object.keys(errors).length;
 
-    useEffect(() => {
-        flash.success && toast.success(flash.success);
+    const currentNavId = useRef(null)
+    const lastShownNavId = useRef(null)
 
-        if (flash.error || numOfErrors > 0) {
-            toast.error(
-                numOfErrors === 1
-                    ? "There is one form error"
-                    : `There are ${numOfErrors} form errors.`
-            );
+    const handleNavigationStart = useCallback(() => {
+        currentNavId.current = Date.now().toString()
+    }, [])
+
+    useEffect(() => {
+        const removeStartEventListener = router.on('start', handleNavigationStart)
+
+        return () => {
+            removeStartEventListener()
+        }
+    }, [handleNavigationStart])
+
+    useEffect(() => {
+        if (currentNavId.current !== lastShownNavId.current) {
+            flash.success && toast.success(flash.success);
+
+            if (flash.error || numOfErrors > 0) {
+                toast.error(
+                    numOfErrors === 1
+                        ? "There is one form error"
+                        : `There are ${numOfErrors} form errors.`
+                );
+            }
+
+            lastShownNavId.current = currentNavId.current
         }
     }, [flash, errors]);
 
