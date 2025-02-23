cfd665b028de3388817429de443e1ebadaf9ef57
Florian <fouteox@gmail.com>
2025-02-23 21:59:08 +0100

    Feature: integrate i18n support in ssr.tsx

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -4,6 +4,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import ReactDOMServer from 'react-dom/server';
 import { RouteName } from 'ziggy-js';
 import { route } from '../../vendor/tightenco/ziggy';
+import './i18n';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -20,6 +21,8 @@ createServer((page) =>
         setup: ({ App, props }) => {
             /* eslint-disable */
             // @ts-expect-error
+            global.i18n = i18n;
+            // @ts-ignore
             global.route<RouteName> = (name, params, absolute) =>
                 route(name, params as any, absolute, {
                     ...page.props.ziggy,
