a7ea74767299ee7cc78cf69510da18784ebb96eb
Florian <fouteox@gmail.com>
2025-02-23 22:05:32 +0100

    Feature: refactor i18n import in ssr.tsx for clarity

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -4,7 +4,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import ReactDOMServer from 'react-dom/server';
 import { RouteName } from 'ziggy-js';
 import { route } from '../../vendor/tightenco/ziggy';
-import './i18n';
+import i18n from './i18n';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -22,7 +22,7 @@ createServer((page) =>
             /* eslint-disable */
             // @ts-expect-error
             global.i18n = i18n;
-            // @ts-ignore
+            // @ts-expect-error
             global.route<RouteName> = (name, params, absolute) =>
                 route(name, params as any, absolute, {
                     ...page.props.ziggy,
