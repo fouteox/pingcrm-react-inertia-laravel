091431d84536f9be59fe501c99e75b5a55392c26
Florian <fouteox@gmail.com>
2025-02-26 15:04:11 +0100

    fix route

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -94,7 +94,7 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
                         </Sheet>
                     </div>
 
-                    <Link href="/dashboard" prefetch className="flex items-center space-x-2">
+                    <Link href={route('dashboard')} prefetch className="flex items-center space-x-2">
                         <AppLogo />
                     </Link>
 
