22de242b2464e4ba712ec0e72c9d2ae90b561fc6
Florian <fouteox@gmail.com>
2026-02-04 22:56:49 +0100

    simplify

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import fadogen from '@fadogen/vite-plugin'
+import fadogen from '@fadogen/vite-plugin';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            host: process.env.VITE_DEV_HOST || 'localhost',
-            clientPort: 443,
-            protocol: 'wss',
-        },
-        origin: process.env.VITE_DEV_HOST ? `https://${process.env.VITE_DEV_HOST}` : undefined,
-        cors: {
-            origin: process.env.APP_HOST ? `https://${process.env.APP_HOST}` : true,
-        },
-        allowedHosts: true,
-        watch: {
-            ignored: [
-                '**/resources/js/actions/**',
-                '**/resources/js/routes/**',
-                '**/resources/js/wayfinder/**',
-            ],
-        },
-    },
     plugins: [
         fadogen(),
         laravel({
